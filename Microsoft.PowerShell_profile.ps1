ed30fd24b495dc84e58f13a8824f8bcb5d8a682a
NDub40 <ewilhelm31@gmail.com>
2024-03-02 11:42:49 -0500

    first commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,36 @@
+Import-Module -Name Terminal-Icons
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+
+# Set UNIX-like aliases for the admin command, so sudo <command> will run the command
+# with elevated rights. 
+Set-Alias -Name su -Value admin
+Set-Alias -Name sudo -Value admin
+
+function ll { Get-ChildItem -Path $pwd -File }
+function find-file($name) {
+    Get-ChildItem -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | ForEach-Object {
+        $place_path = $_.directory
+        Write-Output "${place_path}\${_}"
+    }
+}
+function unzip ($file) {
+    Write-Output("Extracting", $file, "to", $pwd)
+    $fullFile = Get-ChildItem -Path $pwd -Filter .\cove.zip | ForEach-Object { $_.FullName }
+    Expand-Archive -Path $fullFile -DestinationPath $pwd
+}
+function grep($regex, $dir) {
+    if ( $dir ) {
+        Get-ChildItem $dir | select-string $regex
+        return
+    }
+    $input | select-string $regex
+}
+function touch($file) {
+    "" | Out-File $file -Encoding ASCII
+}
+function which($name) {
+    Get-Command $name | Select-Object -ExpandProperty Definition
+}
